13d5c8b5519dfefeca8ba8f05cf909090e3ab362
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-05-25 18:42:45 +0300

    Bind repos with respective interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\Repositories\PornstarRepositoryInterface;
+use App\Repositories\PornstarRepository;
+use App\Interfaces\Repositories\PornstarStatsRepositoryInterface;
+use App\Repositories\PornstarStatsRepository;
+use App\Interfaces\Repositories\AliasRepositoryInterface;
+use App\Repositories\AliasRepository;
+use App\Interfaces\Repositories\ThumbnailRepositoryInterface;
+use App\Repositories\ThumbnailRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PornstarRepositoryInterface::class, PornstarRepository::class);
+        $this->app->bind(PornstarStatsRepositoryInterface::class, PornstarStatsRepository::class);
+        $this->app->bind(AliasRepositoryInterface::class, AliasRepository::class);
+        $this->app->bind(ThumbnailRepositoryInterface::class, ThumbnailRepository::class);
     }
 
     /**

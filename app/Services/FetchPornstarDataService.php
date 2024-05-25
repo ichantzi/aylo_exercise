760885abb208dcb3b6031f9c46c2e0ffa0ca41bc
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-05-25 18:58:38 +0300

    Move fetch data logic to respective service

--- app/Services/FetchPornstarDataService.php
+++ app/Services/FetchPornstarDataService.php
@@ -2,7 +2,19 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class FetchPornstarDataService
 {
+    public function fetchData()
+    {
+        $response = Http::get('https://www.pornhub.com/files/json_feed_pornstars.json');
 
+        if ($response->failed()) {
+            throw new \Exception('Failed to fetch data');
+        }
+
+        return $response->json();
+    }
 }
+

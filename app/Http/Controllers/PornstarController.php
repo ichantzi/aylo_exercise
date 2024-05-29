7307d0570ec3a4dd41e5336abfb89c2d16d1b30f
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-05-29 13:53:30 +0300

    Add PornstarController with show func

---
+++ app/Http/Controllers/PornstarController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\PornstarRepository;
+use Illuminate\Support\Facades\Redis;
+
+class PornstarController extends Controller
+{
+    protected $pornstarRepository;
+
+    public function __construct(PornstarRepository $pornstarRepository)
+    {
+        $this->pornstarRepository = $pornstarRepository;
+    }
+
+    public function show($id)
+    {
+        $pornstar = $this->pornstarRepository->find($id);
+
+        $imageKey = "pornstar_image:{$id}_tablet"; // or whatever type you need
+        $compressedImage = Redis::get($imageKey);
+        $image = gzuncompress($compressedImage);
+
+        return inertia('PornstarShow', [
+            'pornstar' => $pornstar,
+            'image' => base64_encode($image),
+        ]);
+    }
+
+}

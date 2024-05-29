cc3717734db421c63cf93cb1dcb3956bc737d154
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-05-29 14:58:46 +0300

    Create basic display vue component

---
+++ resources/js/Pages/PornstarShow.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>{{ pornstar.name }}</h1>
+        <img :src="`data:image/jpeg;base64,${image}`" alt="Pornstar Image" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pornstar: Object,
+        image: String,
+    },
+};
+</script>

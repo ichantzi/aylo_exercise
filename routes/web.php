8043b3ac67da4d9869459be1a1545f13d95dfe6b
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-05-29 18:04:08 +0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,11 @@ Route::middleware('auth')->group(function () {
 
 use App\Http\Controllers\PornstarController;
 
-Route::get('/pornstars/{id}', [PornstarController::class, 'show']);
+Route::get('/', [PornstarController::class, 'index'])->name('pornstar.index');
+Route::get('/pornstars/{id}', [PornstarController::class, 'show'])->name('pornstar.show');
+Route::get('/api/pornstars/search', [PornstarController::class, 'search'])->name('pornstar.search');
+
+
 
 
 require __DIR__.'/auth.php';

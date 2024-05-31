bbc990487f1e39a8cc4594dd9246b527e139ef46
Ioannis Chantzis <i.chantzis88@gmail.com>
2024-05-31 03:14:41 +0300

    Add arrow keys select functionality

--- resources/js/Components/SearchBar.vue
+++ resources/js/Components/SearchBar.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
+    <div class="relative">
         <input
             type="text"
             v-model="query"
             @input="search"
+            @keydown.down.prevent="navigateSuggestions('down')"
+            @keydown.up.prevent="navigateSuggestions('up')"
+            @keydown.enter.prevent="selectHighlightedSuggestion"
             placeholder="Search for a pornstar..."
             class="h-12 mb-4 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
             style="width: 100%"
         />
-        <ul v-if="suggestions.length" class="bg-white border border-gray-300 w-full max-h-250px overflow-y-auto">
-            <li v-for="suggestion in suggestions" :key="suggestion.id" @click="selectSuggestion(suggestion)" class="px-4 py-2 cursor-pointer hover:bg-gray-100">
+        <ul v-if="suggestions.length" class="absolute left-0 z-10 bg-white border border-gray-300 w-full max-h-250px overflow-y-auto">
+            <li v-for="(suggestion, index) in suggestions"
+                :key="suggestion.id"
+                @click="selectSuggestion(suggestion)"
+                :class="{'bg-gray-100': index === selectedIndex}"
+                class="px-4 py-2 cursor-pointer hover:bg-gray-100">
                 {{ suggestion.name }}
             </li>
         </ul>
@@ -25,16 +32,19 @@ export default {
     setup() {
         const query = ref('');
         const suggestions = ref([]);
+        const selectedIndex = ref(-1);
 
         const search = () => {
             if (query.value.length < 3) {
                 suggestions.value = [];
+                selectedIndex.value = -1;
                 return;
             }
 
             axios.get('/api/pornstars/search', { params: { query: query.value } })
                 .then(response => {
                     suggestions.value = response.data;
+                    selectedIndex.value = -1; // Reset selection when new suggestions are loaded
                 })
                 .catch(error => {
                     console.error(error);
@@ -45,13 +55,34 @@ export default {
             Inertia.get(`/pornstars/${suggestion.id}`);
         };
 
-        return { query, suggestions, search, selectSuggestion };
+        const navigateSuggestions = (direction) => {
+            if (direction === 'down') {
+                if (selectedIndex.value < suggestions.value.length - 1) {
+                    selectedIndex.value += 1;
+                }
+            } else if (direction === 'up') {
+                if (selectedIndex.value > 0) {
+                    selectedIndex.value -= 1;
+                }
+            }
+        };
+
+        const selectHighlightedSuggestion = () => {
+            if (selectedIndex.value !== -1) {
+                selectSuggestion(suggestions.value[selectedIndex.value]);
+            }
+        };
+
+        return { query, suggestions, search, selectSuggestion, navigateSuggestions, selectedIndex, selectHighlightedSuggestion };
     }
 };
 </script>
 
 <style scoped>
-    .max-h-250px {
-        max-height: 250px;
-    }
+.max-h-250px {
+    max-height: 250px;
+}
+.bg-gray-100 {
+    background-color: #f7fafc; /* Tailwind's gray-100 color */
+}
 </style>
